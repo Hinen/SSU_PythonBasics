f24cbdb3255175174fb618712c89417f98ec23b7
Hinen <qkrqldtjqj@naver.com>
2020-09-13 22:10:56 +0900

    Week1 Mission Complete lol

---
+++ Week1Main.py
@@ -0,0 +1,15 @@
+print("택배 배송을 위한 정보를 입력하세요.")
+
+name = input("1) 받는 분 이름: ")
+phoneNumber = input("2) 받는 분 연락처: ")
+address = input("3) 받는 분 주소: ")
+message = input("4) 배송 메시지: ")
+
+print("\n아래 정보로 배송하겠습니다.")
+
+print("==============================================")
+print("- 받는 분 이름 : ", name)
+print("- 받는 분 연락처 : ", phoneNumber)
+print("- 받는 분 주소 : ", address)
+print("- 배송 메시지 : ", message)
+print("==============================================")
